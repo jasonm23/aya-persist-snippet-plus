7caebffe6b362f1831f0031e27e6512a08ed4dc5
jasonm23 <jasonm23@gmail.com>
2016-03-09 07:51:04 +0800

    Rename and use built in custom from auto-yasnipppet

--- auto-yasnippet-persistence+.el
+++ auto-yasnippet-persistence+.el
@@ -1,14 +1,15 @@
-;;; auto-yasnippet-persistence -- persistence extension for auto-yasnippet
+;;; auto-yasnippet-persistence+ -- persistence extension for auto-yasnippet
 
 ;;; Author: Jason Milkins <jasonm23@gmail.com>
+
+;;; Package-Requires: ((auto-yasnippet "0.3"))
 
 ;;; Commentary:
 ;;  [Auto-yasnippet](https://github.com/abo-abo/auto-yasnippet) is a
 ;;  really nice package for quickly creating yasnippets for immediate,
 ;;  throwaway use.  Much like Emacs kmacro features.
 
-;;  I want to have a nice, quick, convenient way to generate
-;;  a suite of snippets I can keep, and build on.
+;;  This package further automates the persist
 
 ;;  Note: This package requires auto-yasnippet,
 
@@ -16,27 +17,16 @@
 
 (require 'auto-yasnippet)
 
-(defgroup auto-yasnippet-persistence nil
-  "Auto YASnippet persistence."
-  :group 'yasnippet)
-
-(defcustom auto-yasnippet-persistence-snippets-dir
-  "~/.emacs.d/snippets"
-  "Directory to save auto snippets, default to `~/.emacs.d/snippets'.
-Persisted snippets will be saved automatically to the sub-folder
-appropriate to the current mode, ie. `../snippets/c' `../snippets/java'
-etc."
-  :type 'directory
-  :group 'auto-yasnippet-persistence)
-
 (defun auto-yasnippet-persistence-persist-snippet ( name key &optional group)
   "Persist the current snippet, NAME KEY and GROUP can be provided by the user.
-The customizable variable, `auto-yasnippet-persistence-snippets-dir' will be
+The customizable variable, `aya-persist-snippets-dir' will be
 used as the root directory for saving snippets.  You will need to
 ensure that yasnippet is also configured to scan this directory
 for snippets.
+
 The current `major-mode` name will be used to determine the snippets
 sub-directory to store the snippet.
+
 For example, if you are in `c-mode`, the snippet will be saved to
 ~/.emacs.d/snippets/c/name, (assuming the default snippets dir.)
 When preceded by `universal-argument' you will be prompted to
@@ -44,7 +34,7 @@ supply a snippet group name.  See
 https://capitaomorte.github.io/yasnippet/snippet-development.html#sec-2-4
 for more on groups.
 It is necessary to use `yas/reload-all' to load/use newly
-persisted snippets."
+persisted snippets. TODO: unless... (use hook)."
   (interactive (if (eq aya-current "")
                    (list nil nil nil)
                  (list
@@ -60,7 +50,7 @@ persisted snippets."
            (mode-snippets-dir nil)
            (snippet-filename nil)
            (groupstring nil)
-           (snippet-dir auto-yasnippet-persistence-snippets-dir)
+           (snippet-dir aya-persist-snippets-dir)
            (modename (format "%s" major-mode))
            (mode-snippets-dir (format "%s/%s"
                                       snippet-dir modename))
@@ -86,6 +76,6 @@ persisted snippets."
            name mode-snippets-dir)
         (append-to-file snippet nil snippet-filename)))))
 
-(provide 'auto-yasnippet-persistence)
+(provide 'auto-yasnippet-persistence+)
 
-;;; auto-yasnippet-persistence.el ends here
+;;; auto-yasnippet-persistence+.el ends here
